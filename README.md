c367a3b111c3943cce65851609591390dfa82e1a
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-08-14 21:33:48 +0200

    Folder Structure Update
    - updating the folder structure to make the reading of the files more easy
    - since ASP.NET is now .NET Core I'm focusing my learning path based on that

--- README.md
+++
@@ -1,2 +0,0 @@
-# C# Design Patterns: Decorator
-This repository contains the course code to accompany the Pluralsight course _C# Design Patterns: Decorator_
---
+++ WeatherInterface/IWeatherService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DecoratorDesignPattern.WeatherInterface
+{
+    public interface IWeatherService
+    {
+        CurrentWeather GetCurrentWeather(String location);
+        LocationForecast GetForecast(String location);
+    }
+}
